6396438d458f4de2b50dc0a2b7321fbbd444342d
platinumpizza29 <kbelgi1@gmail.com>
2025-03-27 12:58:21 +0000

    changed types in sonner.tsx

--- src/components/section-cards.tsx
+++ src/components/section-cards.tsx
@@ -12,7 +12,6 @@ import {
 } from "~/components/ui/card";
 import { auth } from "~/lib/auth";
 import { db } from "~/server/db";
-import type { Hours } from "~/types/hours";
 
 export async function SectionCards() {
   const userData = await auth.api.getSession({
@@ -43,13 +42,15 @@ export async function SectionCards() {
   }, 0);
 
   // get the total revenue for just this month hours+overtime * payrate
-  const totalRevenue = hours.reduce((acc, hour) => {
-    return (
-      acc +
-      parseFloat(hour.hours.toString()) *
-        parseFloat(hour.user.PayRate[0]?.rate.toString() ?? "0")
-    );
-  }, 0);
+  const totalRevenue = hours
+    .reduce((acc, hour) => {
+      return (
+        acc +
+        parseFloat(hour.hours.toString()) *
+          parseFloat(hour.user.PayRate[0]?.rate.toString() ?? "0")
+      );
+    }, 0)
+    .toFixed(3);
 
   return (
     <div className="*:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card grid grid-cols-1 gap-4 px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:shadow-xs lg:px-6 @xl/main:grid-cols-2 @5xl/main:grid-cols-3">
